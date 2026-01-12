5d910f5479a1c07183b14ea6534ace71878e84c7
adotkaya <adotkaya@gmail.com>
2026-01-12 15:38:56 +0300

    Input validation for createMovie and proper error handling for it

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,15 +6,16 @@ import (
 	"time"
 
 	"adotkaya.greenlight.net/internal/data"
+	"adotkaya.greenlight.net/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 	// could have been done with json.Unmarshall too
 	// body, err := io.ReadAll(r.body), if err != nil {app.serverErrorResponse(w,r,err)}
@@ -22,6 +23,19 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 	fmt.Fprintf(w, "%+v\n", input)
